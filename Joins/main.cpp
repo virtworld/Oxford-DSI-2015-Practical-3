13de9b3e000053b44f960e788fd7a6987d080179
Jiaqi Li <alex.jiaqi.li@gmail.com>
2015-03-05 21:07:56 +0000

    added a load bar

--- Joins/main.cpp
+++ Joins/main.cpp
@@ -11,25 +11,37 @@
 #include <sys/select.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <iomanip>
+#include <stdio.h>
 
 int MINIBASE_RESTART_FLAG = 0;// used in minibase part
 
 #define NUM_OF_DB_PAGES  2000 // define # of DB pages
 #define NUM_OF_BUF_PAGES 50 // define Buf manager size.You will need to change this for the analysis
+#define RAW_OUTPUT_ON false
 
 using namespace std;
+
+static inline void loadbar(unsigned int x, unsigned int n, unsigned int w = 50);
 
 int main()
 {
 
 	long count = 0;
-	for(int numOfBuf = NUM_OF_BUF_PAGES; numOfBuf <= NUM_OF_DB_PAGES; numOfBuf += 50)
+	for(int numOfBuf = 16; numOfBuf <= 2048; numOfBuf *= 2)
 	{
-		for(int numOfRecR = 2; numOfRecR < 8192; numOfRecR *= 2)
+		for(int numOfRecR = 2; numOfRecR <= 1024; numOfRecR *= 2)
 		{
-			for(int numOfRecS = 2; numOfRecS < 2048; numOfRecS *= 2)
+			for(int numOfRecS = 2; numOfRecS <= 1024; numOfRecS *= 2)
 			{
-				
+				if(!RAW_OUTPUT_ON)
+				{	
+					count++;
+					int percentage = (int) ((count / (float) (10 * 10 * 8)) * 100);
+					//cout<<percentage<<endl;
+					loadbar(percentage, 100);
+				}
+
 				Status s;
 				
 				minibase_globals = new SystemDefs(s, 
@@ -60,27 +72,54 @@ int main()
 				long pinNo, pinMisses;
 				double duration;
 
-				cout << ">> Number of buffer pages: " << numOfBuf << ", Number of record in R: ";
-				cout << numOfRecR << ", Number of record in S: " << numOfRecS << endl;
-
+				if(RAW_OUTPUT_ON)
+				{
+					cout << ">> Number of buffer pages: " << numOfBuf << ", Number of record in R: ";
+					cout << numOfRecR << ", Number of record in S: " << numOfRecS << endl;
+				}
+				
 				TupleNestedLoopJoin(specOfR, specOfS, pinNo, pinMisses, duration);
 				
-				cout << "TupleNestedLoopJoin: ";
-				cout << "Duration " << duration;
-				cout << ", pinNo " << pinNo;
-				cout << ", pinMisses " << pinMisses << endl;
+				if(RAW_OUTPUT_ON)
+				{
+					cout << "TupleNestedLoopJoin: ";
+					cout << "Duration " << duration;
+					cout << ", pinNo " << pinNo;
+					cout << ", pinMisses " << pinMisses << endl;
+				}
 
 				BlockNestedLoopJoin(specOfR, specOfS, blocksize, pinNo, pinMisses, duration);
 
-				cout << "BlockNestedLoopJoin: ";
-				cout << "Duration " << duration;
-				cout << ", pinNo " << pinNo;
-				cout << ", pinMisses " << pinMisses << endl;
+				if(RAW_OUTPUT_ON)
+				{
+					cout << "BlockNestedLoopJoin: ";
+					cout << "Duration " << duration;
+					cout << ", pinNo " << pinNo;
+					cout << ", pinMisses " << pinMisses << endl;
+				}
 
 				delete minibase_globals;
+				remove( "MINIBASE.DB" ); 
 			}
 	 	}
 	}
 
 	return 1;
+}
+
+/**
+  * Loadbar from: 
+  * https://www.ross.click/2011/02/creating-a-progress-bar-in-c-or-any-other-console-app/
+  */
+static inline void loadbar(unsigned int x, unsigned int n, unsigned int w)
+{
+    if ( (x != n) && (x % (n/100+1) != 0) ) return;
+ 
+    float ratio  =  x/(float)n;
+    int   c      =  ratio * w;
+ 
+    cout << setw(3) << (int)(ratio*100) << "% [";
+    for (int x=0; x<c; x++) cout << "=";
+    for (int x=c; x<w; x++) cout << " ";
+    cout << "]\r" << flush;
 }
