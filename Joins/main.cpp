d02ed130b140ffd92fc0ad583789e468d7cf5475
Jiaqi Li <alex.jiaqi.li@gmail.com>
2015-03-08 02:00:23 +0000

    Changed test cases

--- Joins/main.cpp
+++ Joins/main.cpp
@@ -22,18 +22,18 @@ int MINIBASE_RESTART_FLAG = 0;// used in minibase part
 #define NUM_OF_REC_IN_R_ORIGINAL 10000 // origional settings 
 #define NUM_OF_REC_IN_S_ORIGINAL 2500 // origional settings 
 
-#define NUM_OF_BUF_MAX_PAGES 2048 // for the experiment on different buffer size
+#define NUM_OF_BUF_MAX_PAGES 1024 // for the experiment on different buffer size
 #define NUM_OF_MAX_REC_R 10000 // for the experiment on different R size 
-#define NUM_OF_MAX_REC_S 2500 // for the experiment on different S size
+#define NUM_OF_MAX_REC_S 10000 // for the experiment on different S size
 #define NUM_OF_REPETITION_TASK1 10
 #define NUM_OF_REPETITION_TASK2 5
-#define NUM_OF_REPETITION_TASK3 5
+#define NUM_OF_REPETITION_TASK3 2
 
 using namespace std;
 
 void perfCompTask1();
 void perfCompTask2();
-void perfCompTask3();
+void perfCompTask3(bool isVarR);
 
 void callJoins( int numOfBuf, int numOfRecR, int numOfRecS, 
 	long pinNo[3], long pinMisses[3], double duration[3] );
@@ -47,9 +47,11 @@ static inline void loadbar(unsigned int x, unsigned int n, unsigned int w = 40);
 int main()
 {
 	remove( "data.txt" ); 
-	perfCompTask1();
-	perfCompTask2();
-	perfCompTask3();
+	perfCompTask1(); // orgional settings
+	perfCompTask2(); // variable buffer size
+	perfCompTask3(true); // variable R
+	perfCompTask3(false); // variable S
+	printf("%s\n\n", ">> All done! See data.txt for output.");
 	return 1;
 }
 
@@ -159,55 +161,53 @@ void perfCompTask2()
 // Using relation size, repeat NUM_OF_REPETITION_TASK1 times, 
 // calculate average pin number, pin misses and duration
 //-------------------------------------------------- 
-void perfCompTask3()
+void perfCompTask3(bool varyR)
 {
 	int count = 0;
 	
-	printTestTitle(3, true, "Variant R & S size");
+	printTestTitle(3, true, (varyR) ?"Variant R size" : "Variant S size");
 
-	for(int numOfRecR = 100; numOfRecR <= NUM_OF_MAX_REC_R; numOfRecR *= 10)
+	for(int numOfRec = 2; numOfRec <= ((varyR) ? NUM_OF_MAX_REC_R : NUM_OF_MAX_REC_S); numOfRec *= 4)
 	{
-		for(int numOfRecS = 25; numOfRecS <= NUM_OF_MAX_REC_S && numOfRecS < numOfRecR; numOfRecS *= 10)
+		printSettings(NUM_OF_BUF_PAGES_ORIGINAL, (varyR) ? numOfRec : NUM_OF_REC_IN_R_ORIGINAL, 
+			(varyR) ? NUM_OF_REC_IN_S_ORIGINAL : numOfRec);
+
+		double *avgPinNo = new double[3];
+		double *avgPinMisses = new double[3];
+		double *avgDuration = new double[3];
+
+		for(int numOfRepetition = 0; numOfRepetition < NUM_OF_REPETITION_TASK3; numOfRepetition++)
 		{
-			printSettings(NUM_OF_BUF_PAGES_ORIGINAL, numOfRecR, numOfRecS);
-
-			double *avgPinNo = new double[3];
-			double *avgPinMisses = new double[3];
-			double *avgDuration = new double[3];
-
-			for(int numOfRepetition = 0; numOfRepetition < NUM_OF_REPETITION_TASK3; numOfRepetition++)
-			{
 				// display progress bar
-				int percentage = (int) ((count / (float) (NUM_OF_REPETITION_TASK3 * 6)) * 100);
-				loadbar(percentage, 100);
-				if(percentage == 100) cout << endl;
+			int percentage = (int) ((count / (float) (NUM_OF_REPETITION_TASK3 * 8)) * 100);
+			loadbar(percentage, 100);
+			if(percentage == 100) cout << endl;
 
 				// do joins
-				long *pinNo = new long[3];
-				long *pinMisses = new long[3];
-				double *duration = new double[3];
+			long *pinNo = new long[3];
+			long *pinMisses = new long[3];
+			double *duration = new double[3];
 
-				callJoins( NUM_OF_BUF_PAGES_ORIGINAL, numOfRecR, numOfRecS, 
-					pinNo, pinMisses, duration);
+			callJoins( NUM_OF_BUF_PAGES_ORIGINAL, (varyR) ? numOfRec : NUM_OF_REC_IN_R_ORIGINAL, 
+				(varyR) ? NUM_OF_REC_IN_S_ORIGINAL : numOfRec, pinNo, pinMisses, duration);
 
-				for(int i = 0; i < 3; i++)
-				{
-					avgPinNo[i] = (avgPinNo[i] * numOfRepetition + pinNo[i]) / (numOfRepetition + 1);
-					avgPinMisses[i] = (avgPinMisses[i] * numOfRepetition + pinMisses[i]) / (numOfRepetition + 1);
-					avgDuration[i] = (avgDuration[i] * numOfRepetition + duration[i]) / (numOfRepetition + 1);
-				}
-
-				delete[] pinNo, pinMisses, duration;
-				count++;
+			for(int i = 0; i < 3; i++)
+			{
+				avgPinNo[i] = (avgPinNo[i] * numOfRepetition + pinNo[i]) / (numOfRepetition + 1);
+				avgPinMisses[i] = (avgPinMisses[i] * numOfRepetition + pinMisses[i]) / (numOfRepetition + 1);
+				avgDuration[i] = (avgDuration[i] * numOfRepetition + duration[i]) / (numOfRepetition + 1);
 			}
 
-			printResults( avgPinNo, avgPinMisses, avgDuration);
-			delete[] avgPinNo, avgPinMisses, avgDuration;
-			cout<< endl;
+			delete[] pinNo, pinMisses, duration;
+			count++;
 		}
+
+		printResults( avgPinNo, avgPinMisses, avgDuration);
+		delete[] avgPinNo, avgPinMisses, avgDuration;
+		cout<< endl;
 	}
 
-	printTestTitle(3, false, "Variant R & S size");
+	printTestTitle(3, false, (varyR) ? "Variant R size" : "Variant S size");
 }
 
 void callJoins( int numOfBuf, int numOfRecR, int numOfRecS, 
@@ -254,8 +254,8 @@ void printTestTitle(int testNo, bool isStart, const char* nameOfTest)
 	}
 	else
 	{
-		printf("%s%d%s\n\n", ">>----------End of Test ", testNo, "----------");
-		fprintf(pFile, "%s%d%s\n\n", ">>----------End of Test ", testNo, "----------");
+		printf("%s%d%s%s%s\n\n", ">>----------End of Test ", testNo, ": ", nameOfTest, "----------");
+		fprintf(pFile, "%s%d%s%s%s\n\n", ">>----------End of Test ", testNo, ": ", nameOfTest, "----------");
 	}
 
 	fclose(pFile);
@@ -292,7 +292,7 @@ void printResults(double avgPinNo[3], double avgPinMisses[3], double avgDuration
   */
   static inline void loadbar(unsigned int x, unsigned int n, unsigned int w)
   {
-  	if ( (x != n) && (x % (n/100+1) != 0) ) return;
+  	//if ( (x != n) && (x % (n/100+1) != 0) ) return;
   	float ratio  =  x/(float)n;
   	int   c      =  ratio * w;
 
